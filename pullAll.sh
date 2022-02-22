6258b0aa1769b291b53c23572b19cd3fe0360751
huangcy <huangcy@autoai.com>
2022-02-22 10:47:44 +0800

    add pullAll.sh

---
+++ pullAll.sh
@@ -0,0 +1,31 @@
+bash pull.sh
+
+cd ../Algorithm
+bash pull.sh
+
+cd ../BoostTest
+bash pull.sh
+
+cd ../CmakeTest
+bash pull.sh
+
+cd ../CPPTest
+bash pull.sh
+
+cd ../log4js-demo
+bash pull.sh
+
+cd ../mocha_integrative_test
+bash pull.sh
+
+cd ../plan
+bash pull.sh
+
+cd ../unixProgramming
+bash pull.sh
+
+cd ../wangguilin
+bash pull.sh
+
+cd ../winston_demo
+bash pull.sh
