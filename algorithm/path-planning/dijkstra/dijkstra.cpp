f350139336ad81a73c80efa89b4b3d41441f755c
huangcy <huangcy@autoai.com>
2022-06-28 19:08:12 +0800

    update dijkstra

--- algorithm/path-planning/dijkstra/dijkstra.cpp
+++ algorithm/path-planning/dijkstra/dijkstra.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <string>
+#include <queue>
+#include <unordered_map>
+#include <stdint.h>
 
 using namespace std;
 
@@ -15,23 +19,92 @@ using namespace std;
     4->6: 15
     5->6: 4
 
-       1  2  3  4  5  6
-    1  0  1  12 &  &  &
-    2  &  0  9  3  &  &
-    3  &  &  0  &  5  &  
-    4  &  &  4  0  13 15
-    5  &  &  &  &  &  4
-    6  &  &  &  &  &  0
+       0  1  2  3  4  5
+    0  0  1  12 &  &  &
+    1  &  0  9  3  &  &
+    2  &  &  0  &  5  &  
+    3  &  &  4  0  13 15
+    4  &  &  &  &  &  4
+    5  &  &  &  &  &  0
 */
 
-void initGraph()
+class Node
 {
+public:
+    Node() : parent(NULL), cost(UINT32_MAX) {}
 
+    bool operator<(const Node& r) { return this->cost < r.cost; }
+    Node *parent;
+    uint32_t cost;
+    std::string name;
+};
+
+class dijkstra
+{
+private:
+    int graph[6][6];
+    std::unordered_map<std::string, Node> nodes;
+    std::priority_queue<Node> openSet;
+
+public:
+    dijkstra() {}
+    ~dijkstra() {}
+
+    void setOrigin(Node& node)
+    {
+        openSet.push(node);
+    }
+
+    void setDestination()
+    {
+
+    }
+};
+
+void initGraph(int (*graph)[6])
+{
+    for (size_t i = 0; i < 6; i++)
+    {
+        for (int j = 0; j < 6; j++)
+        {
+            graph[i][j] = INT32_MAX;
+        }
+    }
+
+    graph[0][1] = 1;
+    graph[0][2] = 12;
+    graph[1][2] = 9;
+    graph[1][3] = 3;
+    graph[2][4] = 5;
+    graph[3][2] = 4;
+    graph[3][4] = 13;
+    graph[3][5] = 15;
+    graph[4][5] = 4;
 }
 
-void dijkstra()
+void initNode(std::unordered_map<std::string, Node> &nodes)
 {
+    for (size_t i = 0; i < nodes.size(); i++)
+    {
+        Node node;
+        node.name = 'A' + i;
+        nodes[node.name] = node;
+    }
+}
 
+void test_dijkstra()
+{
+    int graph[6][6];
+    std::unordered_map<std::string, Node> nodes;
+    std::priority_queue<Node> openSet;
+
+    initGraph(graph);
+    while (1)
+    {
+        const Node& minNode = openSet.top();
+        openSet.pop();
+    }
+    
 }
 
 int main()
