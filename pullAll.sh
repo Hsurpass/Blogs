bed77108434ea8ba8315537a9b3568709a1264b2
huangcy <huangcy@autoai.com>
2022-07-26 11:44:56 +0800

    update

--- pullAll.sh
+++ pullAll.sh
@@ -1,34 +1,36 @@
-bash pull.sh
+#!/bin/bash
+
+./pull.sh
 
 cd ../Algorithm
-bash pull.sh
+./pull.sh
 
 cd ../BoostTest
-bash pull.sh
+./pull.sh
 
 cd ../CmakeTest
-bash pull.sh
+./pull.sh
 
 cd ../CPPTest
-bash pull.sh
+./pull.sh
 
 cd ../log4js-demo
-bash pull.sh
+./pull.sh
 
 cd ../mocha_integrative_test
-bash pull.sh
+./pull.sh
 
 cd ../plan
-bash pull.sh
+./pull.sh
 
 cd ../unixProgramming
-bash pull.sh
+./pull.sh
 
 cd ../wangguilin
-bash pull.sh
+./pull.sh
 
 cd ../winston_demo
-bash pull.sh
+./pull.sh
 
 cd ../houjie
-bash pull.sh
+./pull.sh
