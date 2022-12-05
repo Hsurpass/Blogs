fa8785e860c33823a53c70c60ffb771485617ecb
hchy <1742143252@qq.com>
2022-12-05 18:14:44 +0800

    rm protobuf

--- protobuf/demo/proto/compile.bat
+++
@@ -1,5 +0,0 @@
-protoc.exe -I. --cpp_out=../pb addressbook.proto
-protoc.exe -I. --cpp_out=../pb person.proto
-protoc.exe -I. --cpp_out=../pb SDPath.proto
-protoc.exe -I. --cpp_out=../pb testChinese.proto
-protoc.exe -I. --cpp_out=../pb testoneof.proto
